1d1ee5588817d9a2a9ec1c92ea130b8c11949807
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-05 01:49:03 +0100

    hhdhd

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { gtData } from './banks-data/gt';
+import { kudaData } from './banks-data/kuda';
+import { ubaData } from './banks-data/uba';
 
 @Injectable()
 export class AppService {
@@ -9,8 +11,12 @@ export class AppService {
 
   connectBank(accountNo: string, bankName: string): any {
     switch (bankName) {
-      case 'gtBank':
+      case 'gt':
         return gtData;
+      case 'kuda':
+        return kudaData;
+      case 'uba':
+        return ubaData;
       default:
         return 'Bank not found';
     }
