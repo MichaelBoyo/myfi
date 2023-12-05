b3678d81ace7ca3dd544d548fb149d665ed9804d
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-05 01:27:54 +0100

    Update start script to use node dist/main

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { gtData } from './banks-data/gt';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  connectBank(accountNo: string, bankName: string): any {
+    switch (bankName) {
+      case 'gtBank':
+        return gtData;
+      default:
+        return 'Bank not found';
+    }
   }
 }
